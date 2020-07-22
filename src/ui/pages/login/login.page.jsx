4a994db6ec46affb0ec546c617bc006522584aca
Lucas Rohr <lucas.carreno@cwi.com.br>
2020-07-22 19:41:47 -0300

    Adding base folders

---
+++ src/ui/pages/login/login.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Login = () => {
+  return <div className="login-container"> xaxaxaxa </div>
+}
+
+Login.propTypes = {}
+
+export { Login }
